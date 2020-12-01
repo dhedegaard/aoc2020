47793729c3b676a742f07744dcd06c8f090598eb
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-12-01 16:37:30 +0100

    day01: Both solutions.

--- aoc2020/src/day01/mod.rs
+++ aoc2020/src/day01/mod.rs
@@ -1,13 +1,63 @@
+fn parse(input: &str) -> Vec<u32> {
+    input
+        .split_whitespace()
+        .map(|e| e.trim().parse().unwrap())
+        .collect()
+}
+
+pub fn get_input() -> String {
+    include_str!("./input.txt").to_owned()
+}
+
 pub fn part1(input: &str) -> String {
-    input.to_owned()
+    let numbers = parse(input);
+    for e in &numbers {
+        for f in &numbers {
+            if e + f == 2020 {
+                return (e * f).to_string();
+            }
+        }
+    }
+    unreachable!("No valid solution");
+}
+
+pub fn part2(input: &str) -> String {
+    let numbers = parse(input);
+    for e in &numbers {
+        for f in &numbers {
+            for g in &numbers {
+                if e + f + g == 2020 {
+                    return (e * f * g).to_string();
+                }
+            }
+        }
+    }
+    unreachable!("No valid solution");
 }
 
 #[cfg(test)]
 mod tests {
+    const TEST_INPUT: &str = include_str!("./test_input.txt");
+
     use super::*;
 
     #[test]
-    fn should_return_input() {
-        assert_eq!(part1(&"test"), "test");
+    fn part1_example() {
+        assert_eq!(part1(TEST_INPUT), "514579");
+    }
+
+    #[test]
+    fn part1_result() {
+        assert_eq!(part1(&get_input()), "889779");
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(TEST_INPUT), "241861950");
+    }
+
+    #[test]
+    fn part2_result() {
+        assert_eq!(part2(&get_input()), "76110336");
     }
 }
