af00fc02d81efabf7507a8f7358d53ddb0502572
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-12-01 22:34:17 +0100

    Added head element.

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,9 +1,17 @@
+import Head from "next/head";
 import { AocProvider } from "../contexts/aoc";
 
 const App = ({ Component, pageProps }: any) => (
-  <AocProvider>
-    <Component {...pageProps} />
-  </AocProvider>
+  <>
+    <Head>
+      <title>Advent of Code 2020 - solutions</title>
+      <meta name="Description" content="Solutions for Advent of Code 2020." />
+    </Head>
+
+    <AocProvider>
+      <Component {...pageProps} />
+    </AocProvider>
+  </>
 );
 
 export default App;
