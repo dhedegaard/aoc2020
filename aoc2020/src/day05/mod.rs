d566a91cadec281bdad5f8115a3afa4e29a8d42d
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-12-05 22:45:07 +0100

    day05: part2

--- aoc2020/src/day05/mod.rs
+++ aoc2020/src/day05/mod.rs
@@ -1,23 +1,38 @@
+use std::collections::BTreeSet;
+
 pub fn get_input() -> String {
     include_str!("./input.txt").to_string()
+}
+
+fn lineToSeatId(line: &str) -> i32 {
+    let mut rows = [0, 127];
+    let mut columns = [0, 7];
+    line.chars().for_each(|c| match c {
+        'B' => rows[0] += (rows[1] - rows[0] + 1) / 2,
+        'F' => rows[1] -= (rows[1] - rows[0] + 1) / 2,
+        'R' => columns[0] += (columns[1] - columns[0] + 1) / 2,
+        'L' => columns[1] -= (columns[1] - columns[0] + 1) / 2,
+        _ => unreachable!(),
+    });
+    rows[0] * 8 + columns[0]
 }
 
 pub fn part1(input: &str) -> String {
     input
         .lines()
-        .map(|line| {
-            let mut rows = [0, 127];
-            let mut columns = [0, 7];
-            line.chars().for_each(|c| match c {
-                'B' => rows[0] += (rows[1] - rows[0] + 1) / 2,
-                'F' => rows[1] -= (rows[1] - rows[0] + 1) / 2,
-                'R' => columns[0] += (columns[1] - columns[0] + 1) / 2,
-                'L' => columns[1] -= (columns[1] - columns[0] + 1) / 2,
-                _ => unreachable!(),
-            });
-            rows[0] * 8 + columns[0]
-        })
+        .map(|line| lineToSeatId(line))
         .max()
+        .unwrap()
+        .to_string()
+}
+
+pub fn part2(input: &str) -> String {
+    let seat_ids = input
+        .lines()
+        .map(|line| lineToSeatId(line))
+        .collect::<BTreeSet<_>>();
+    (*seat_ids.iter().min().unwrap()..*seat_ids.iter().max().unwrap())
+        .find(|&seat_id| !seat_ids.contains(&seat_id))
         .unwrap()
         .to_string()
 }
@@ -37,5 +52,10 @@ mod tests {
     #[test]
     fn part1_result() {
         assert_eq!(part1(&get_input()), "976");
+    }
+
+    #[test]
+    fn part2_result() {
+        assert_eq!(part2(&get_input()), "685");
     }
 }
